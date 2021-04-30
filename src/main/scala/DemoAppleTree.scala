74c14c8d3c01d013ededeee4db45202d35d0a83b
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-04-30 16:49:05 +0200

    Added example

---
+++ src/main/scala/DemoAppleTree.scala
@@ -0,0 +1,43 @@
+import Pots._
+import cats.syntax.all._
+
+object DemoAppleTree extends App {
+
+  val yes = 1
+  val no = 0
+
+  val isApple = Variable(0, "isApple", List(yes, no))
+  val isFruit = Variable(1, "isFruit", List(yes, no))
+  val growsOnTree = Variable(2, "growsOnTree", List(yes, no))
+
+  // p(isFruit|isApple)
+  val allApplesAreFruitsPot = Potential(
+    List(isFruit, isApple),
+    Map(
+      List(yes, yes) -> 1.0,
+      List(no, yes) -> 0.0,
+      List(yes, no) -> 0.5,
+      List(no, no) -> 0.5,
+    )
+  )
+
+  // p(growsOnTree|isFruit)
+  val allFruitsGrowOnTreesPot = Potential(
+    List(growsOnTree, isFruit),
+    Map(
+      List(yes, yes) -> 1.0,
+      List(no, yes) -> 0.0,
+      List(yes, no) -> 0.5,
+      List(no, no) -> 0.5,
+    )
+  )
+
+  val jointPot = multpots(allApplesAreFruitsPot, allFruitsGrowOnTreesPot)
+
+  disppot(jointPot)
+
+  // all apples grow on trees
+  println("p(growsOnTree|isApple=yes)")
+  disppot((sumpot(isFruit) >>> condpot(isApple) >>> setpot(isApple, yes))(jointPot))
+
+}
